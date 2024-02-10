4edf4797ee7b388747860c6df2dfd7d4caef7fee
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2024-02-10 23:55:31 +0300

    sinir cizgileri

---
+++ coding/06_contours_convex-hull_convexity-defects.py/00_tanimlar.py
@@ -0,0 +1,17 @@
+# şekli çevreleyen sınırları
+
+# kontur (contour)
+"""
+Önce binary'ye çevirmeliyiz
+sonra koordinatlarını bulmalıyız
+sonra bu noktaları çizmeliyiz
+"""
+
+# convex hull
+"""
+Dışbükey Örtü
+dışa doğru bükülme
+iç bükey şekillere dış bükey şekiller çizmek !!
+"""
+
+# Convexity Defects: Dışbükey kusurlar
