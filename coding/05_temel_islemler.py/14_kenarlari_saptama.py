46067bad8e46cabdc9258a56fdba3943e5fc5a13
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2024-02-10 16:20:15 +0300

    Create 14_kenarlari_saptama.py

---
+++ coding/05_temel_islemler.py/14_kenarlari_saptama.py
@@ -0,0 +1,18 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    
+    edges = cv2.Canny(frame, 100, 200)
+
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Edges", edges)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
